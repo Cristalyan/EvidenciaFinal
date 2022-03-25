4babec32be6dddf12525b23064bea2280b54368a
Alumno <Alumno@ED1-CC2-06>
2022-03-25 13:52:42 -0600

    Evidencia

---
+++ src/CodigoFuente/Cliente.java
@@ -0,0 +1,19 @@
+package CodigoFuente;
+
+public class Cliente extends Personas {
+
+	public static String Licencia;
+
+	
+	
+		public static String getLicencia() {
+		return Licencia;
+	}
+
+		public static void setLicencia(String licencia) {
+		Licencia = licencia;
+	}
+	
+	
+	
+}
---
+++ src/CodigoFuente/MainRenta.java
@@ -0,0 +1,21 @@
+package CodigoFuente;
+
+import java.util.ArrayList;
+
+public class MainRenta {
+
+	public static ArrayList<Empleado> Lista_Empleado= new ArrayList<Empleado>();
+	public static ArrayList<Cliente> Lista_Cliente= new ArrayList<Cliente>();
+	public static ArrayList<Acuaticos> Lista_Acuaticos= new ArrayList<Acuaticos>();
+	public static ArrayList<Aereos> Lista_Aereos= new ArrayList<Aereos>();
+	public static ArrayList<Terrestres> Lista_Terrestres= new ArrayList<Terrestres>();
+	public static ArrayList<registrorenta> Lista_registrorenta= new ArrayList<registrorenta>();
+	public static ArrayList<Personas> Lista_Personas= new ArrayList<Personas>();
+	public static ArrayList<Vehículos> Lista_Vehículos= new ArrayList<Vehículos>();
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
